64f0dab85285f604cf64186110cc46335d1aa1ad
kriengkai <kriengkai.s@ku.th>
2026-04-27 20:31:20 +0700

    first commit

---
+++ src/App.test.tsx
@@ -0,0 +1,8 @@
+import '@testing-library/jest-dom'
+import {render, screen} from '@testing-library/react'
+import App from './App'
+
+test('renders heading', () => {
+  render(<App />)
+  expect(screen.getByText(/React Demo App/i)).toBeInTheDocument()
+})
---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div style={{padding: 40}}>
+        <h1>React Demo App Built for Github Action Testing</h1>
+        <p>Github Action Testing</p>
+        
+      </div>
+    </>
+  )
+}
+
+export default App
