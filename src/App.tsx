84669b8c0884c025a8d775d3c2f48c63b92705ed
kriengkai <kriengkai.s@ku.th>
2026-04-28 10:49:49 +0700

    change .tsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <div style={{padding: 40}}>
-        <h1>React Demo App Built for Github Action Testing</h1>
+        <h1>React Demo App Built for Github Action Testing!</h1>
         <p>Github Action Testing</p>
         
       </div>
